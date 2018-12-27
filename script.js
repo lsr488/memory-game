94d938f32231a1409ea3237d58fb8edad97827e9
Lisa <lsr488@gmail.com>
2018-12-26 19:22:40 -0500

    Added card data into an array.

--- script.js
+++ script.js
@@ -9,3 +9,55 @@
 // finished game
 // end-of-game stats?
 // reset button?
+
+// Card Data
+const cardsArray = [
+	{
+		"name": "shell",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/blueshell.png",
+	},
+	{
+		"name": "star",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/star.png",
+	},
+	{
+		"name": "bobomb",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/bobomb.png",
+	},
+	{
+		"name": "mario",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/mario.png",
+	},
+	{
+		"name": "luigi",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/luigi.png",
+	},
+	{
+		"name": "peach",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/peach.png",
+	},
+	{
+		"name": "1up",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/1up.png",
+	},
+	{
+		"name": "mushroom",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/mushroom.png",
+	},
+	{
+		"name": "thwomp",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/thwomp.png",
+	},
+	{
+		"name": "bulletbill",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/bulletbill.png",
+	},
+	{
+		"name": "coin",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/coin.png",
+	},
+	{
+		"name": "goomba",
+		"img": "https://raw.githubusercontent.com/taniarascia/memory/master/img/goomba.png",
+	},
+];

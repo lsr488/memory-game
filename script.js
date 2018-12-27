1c77143fb944347abfe963cea990c59aef98c5c7
Lisa <lsr488@gmail.com>
2018-12-26 19:09:00 -0500

    Initial brainstorming.

---
+++ script.js
@@ -0,0 +1,11 @@
+// display 12 cards
+// duplicate cards to have 2 sets of 12
+// randomzie card display
+// add selected syle for selected cards
+// only allow 2 cards to be selected at a time
+// reset guess count after 2
+// add delay to selections
+// show back of card initially and flip on select
+// finished game
+// end-of-game stats?
+// reset button?
